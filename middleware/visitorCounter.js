c0ed28cd95097cd5fde498ff0b27b6896e526843
Elçin Erdemir <elcix71@gmail.com>
2025-12-28 18:21:43 +0300

    fix: count daily unique visits by ip and ua

--- middleware/visitorCounter.js
+++ middleware/visitorCounter.js
@@ -2,8 +2,6 @@ const crypto = require("crypto");
 const db = require("../model/data");
 const config = require("../config");
 
-const TWO_HOURS_MS = 2 * 60 * 60 * 1000;
-
 const hashValue = (value) => {
     return crypto.createHash("sha256").update(value).digest("hex");
 };
@@ -17,15 +15,12 @@ module.exports = async (req, res, next) => {
         const uaHash = hashValue(`${ua}:${salt}`);
 
         const [[last]] = await db.execute(
-            "SELECT visited_at FROM visit_logs WHERE ip_hash=? AND ua_hash=? ORDER BY visited_at DESC LIMIT 1",
+            "SELECT visited_at FROM visit_logs WHERE ip_hash=? AND ua_hash=? AND DATE(visited_at)=CURDATE() LIMIT 1",
             [ipHash, uaHash]
         );
         if (last && last.visited_at) {
-            const lastTime = new Date(last.visited_at).getTime();
-            if (Date.now() - lastTime < TWO_HOURS_MS) {
-                req.visitLogged = false;
-                return next();
-            }
+            req.visitLogged = false;
+            return next();
         }
 
         await db.execute(
