07d9339e2319051ee13e70c3c0d4e4fb8d910647
Elçin Erdemir <elcix71@gmail.com>
2025-12-29 01:24:57 +0300

    debug(visitorCounter): add logging to see if middleware runs and IP detection

--- middleware/visitorCounter.js
+++ middleware/visitorCounter.js
@@ -12,16 +12,19 @@ module.exports = async (req, res, next) => {
         const ua = req.headers["user-agent"] || "";
         const salt = config.sessionSecret || "visitor";
         const ipHash = hashValue(`${ip}:${salt}`);
+        console.log("[visitorCounter] ip:", ip, "path:", req.path);
         // Remove UA dependency: count only unique IP per day
         const [[last]] = await db.execute(
             "SELECT visited_at FROM visit_logs WHERE ip_hash=? AND DATE(visited_at)=CURDATE() LIMIT 1",
             [ipHash]
         );
         if (last && last.visited_at) {
+            console.log("[visitorCounter] already visited today");
             req.visitLogged = false;
             return next();
         }
 
+        console.log("[visitorCounter] inserting new visit");
         await db.execute(
             "INSERT INTO visit_logs (ip_hash, ua_hash, visited_at) VALUES (?,?,NOW())",
             [ipHash, hashValue(`${ua}:${salt}`)]
@@ -29,6 +32,7 @@ module.exports = async (req, res, next) => {
         req.visitLogged = true;
         next();
     } catch (err) {
+        console.error("[visitorCounter] error:", err);
         next(err);
     }
 };
